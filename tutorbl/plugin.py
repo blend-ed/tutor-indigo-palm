8f95edf5f1c58031b91f221ea29c125bfab43843
ZAMEEL HASSAN <zekyazelu7@gmail.com>
2024-02-10 03:23:53 +0530

    Add npm install for brand-bl-v2 git repository

--- tutorbl/plugin.py
+++ tutorbl/plugin.py
@@ -103,37 +103,29 @@ hooks.Filters.CONFIG_UNIQUE.add_items(
 hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
 
 
+# hooks.Filters.ENV_PATCHES.add_item(
+#     (
+#         "mfe-dockerfile-post-npm-install",
+#         """
+#         # using git repo of brand
+#         RUN npm install '@edx/brand@git+https://github.com/blend-ed/brand-bl-v2.git'
+#         """
+#     )
+# )
+
 hooks.Filters.ENV_PATCHES.add_items(
     [
         (
-            "mfe-dockerfile-post-npm-install-learning",
+            "mfe-dockerfile-post-npm-install",
             """
-RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^1.0.0'
-RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^1.0.0'
-RUN npm install '@edx/frontend-component-footer@npm:@edly-io/indigo-frontend-component-footer@^1.0.0'
-""",
+# using git repo of brand
+RUN npm install '@edx/brand@git+https://github.com/blend-ed/brand-bl-v2.git'
+        """
         ),
         (
             "mfe-dockerfile-post-npm-install-authn",
             """
 RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^1.0.0'
-""",
-        ),
-        # Tutor-Indigo v2.1 targets the styling updations in discussions and learner-dashboard MFE
-        # brand-openedx is related to styling updates while others are for header and footer updates
-        (
-            "mfe-dockerfile-post-npm-install-discussions",
-            """
-RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^1.0.0'
-RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^1.0.0'
-RUN npm install '@edx/frontend-component-footer@npm:@edly-io/indigo-frontend-component-footer@^1.0.0'
-""",
-        ),
-        (
-            "mfe-dockerfile-post-npm-install-learner-dashboard",
-            """
-RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^1.0.0'
-RUN npm install '@edx/frontend-component-footer@npm:@edly-io/indigo-frontend-component-footer@^1.0.0'
 """,
         ),
     ]
